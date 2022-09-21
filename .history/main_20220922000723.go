2678234c5b5851ac4477e17de3c935b62c41f224
Arif00 <arif.cihangir@yh.nackademin.se>
2022-09-22 00:08:29 +0200

    firsttest

--- .history/main_20220922000723.go
+++ .history/main_20220922000723.go
@@ -17,7 +17,7 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
-	if len(os.Args) != 2 {
+	if len(os.Args != 2) {
 		printError(errInvalidArguments)
 	}
 	for {
